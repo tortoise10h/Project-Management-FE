33d4a649107d56d45cb0a5f6e3eed0e431e0ba78
LuongTuLinh <hoailinhkt2015@gmail.com>
2019-09-25 21:43:08 +0700

    abc

--- src/common/components/LandingPage.js
+++ src/common/components/LandingPage.js
@@ -19,7 +19,7 @@ export default class LandingPage extends Component {
                     <img src={LogoBanana} alt='' />
                   </a>
                 </Col>
-                <Col className='nav-right text-center text-lg-right' span={12} offset={6} style={{ textAlign: 'right'}}>
+                <Col className='nav-right text-center text-lg-right' span={12} offset={6} style={{ textAlign: 'right' }}>
                   <a className='button-login' href='#'>Log in</a>
                   <a className='button button-signup' href='#'>Sign up</a>
                 </Col>
@@ -57,7 +57,7 @@ export default class LandingPage extends Component {
                 <p>Fowl have fruit moveth male they are that place you will lesser</p>
               </div>
               <Row>
-                <Col className='col-md-6 col-lg-4 mb-4 mb-lg-0'  md={{ span: 12 }} lg={{ span: 8 }}>
+                <Col className='col-md-6 col-lg-4 mb-4 mb-lg-0' md={{ span: 12 }} lg={{ span: 8 }}>
                   <div className='service-card text-center'>
                     <div className='service-card-img'>
                       <img className='img-fluid' src='img/home/service1.png' alt='' />
@@ -68,7 +68,7 @@ export default class LandingPage extends Component {
                     </div>
                   </div>
                 </Col>
-                <Col className='col-md-6 col-lg-4 mb-4 mb-lg-0'  md={{ span: 12 }} lg={{ span: 8 }}>
+                <Col className='col-md-6 col-lg-4 mb-4 mb-lg-0' md={{ span: 12 }} lg={{ span: 8 }}>
                   <div className='service-card text-center'>
                     <div className='service-card-img'>
                       <img className='img-fluid' src='img/home/service2.png' alt='' />
@@ -79,7 +79,7 @@ export default class LandingPage extends Component {
                     </div>
                   </div>
                 </Col>
-                <Col className='col-md-6 col-lg-4 mb-4 mb-lg-0'  md={{ span: 12 }} lg={{ span: 8 }}>
+                <Col className='col-md-6 col-lg-4 mb-4 mb-lg-0' md={{ span: 12 }} lg={{ span: 8 }}>
                   <div className='service-card text-center'>
                     <div className='service-card-img'>
                       <img className='img-fluid' src='img/home/service3.png' alt='' />
@@ -98,17 +98,17 @@ export default class LandingPage extends Component {
           <section className='bg-gray section-padding magic-ball magic-ball-about'>
             <div className='container'>
               <Row className='row'>
-                <Col className='mb-4 mb-md-0'  md={{ span: 10 }} lg={{ span: 12 }}>
+                <Col className='mb-4 mb-md-0' md={{ span: 10 }} lg={{ span: 12 }}>
                   <div className='about-img'>
                     <div className='board'>
                       <div className='header'>
                         <div className='title'>
-                            <h3>Team Tasks</h3>
+                          <h3>Team Tasks</h3>
                         </div>
                       </div>
                       <div className='lists'>
                         <div className='list'>
-                            <div className='list-content'>
+                          <div className='list-content'>
                             <h5>Doing</h5>
                             <div className='card' data-aos='fade-right' data-aos-delay={500}>
                               <div className='card-content'>
@@ -167,9 +167,9 @@ export default class LandingPage extends Component {
                             <div className='add-card'> Add Todo..
                             </div>
                           </div>
-                          </div>
+                        </div>
                         <div className='list'>
-                            <div className='list-content'>
+                          <div className='list-content'>
                             <h5>Done</h5>
                             <div className='card' data-aos='fade-left' data-aos-delay={500}>
                               <div className='card-content'>
@@ -211,12 +211,12 @@ export default class LandingPage extends Component {
                             <div className='add-card'> Add Todo...
                             </div>
                           </div>
-                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </Col>
-                <Col className='about-content' md={{ span: 14 }} lg={{ span: 12 }} style={{ position: 'relative', alignSelf: 'center'}}>
+                <Col className='about-content' md={{ span: 14 }} lg={{ span: 12 }} style={{ position: 'relative', alignSelf: 'center' }}>
                   <h2>Work with any team</h2>
                   <p>Whether it’s for work, a side project or even the next family vacation, Our helps your team stay organized.</p>
                   <a className='button' href='#'>Start Doing   →</a>
@@ -228,74 +228,74 @@ export default class LandingPage extends Component {
           {/* ================Tour section Start ================= */}
           <section className='section-margin pb-xl-5'>
             <div className='container'>
-              <div className='row'>
-                <div className='col-md-6'>
+              <Row className='row'>
+                <Col className='col-md-6' md={{ span: 12 }}>
                   <div className='tour-card'>
                     <img className='card-img rounded-0' src='img/home/tour1.png' alt='' />
                     <div className='tour-card-overlay'>
                       <div className='media'>
                         <div className='media-body'>
-                            <h4>Running real time</h4>
-                            <small>5 days offer</small>
-                            <p>We proper guided our tourist</p>
-                          </div>
+                          <h4>Running real time</h4>
+                          <small>5 days offer</small>
+                          <p>We proper guided our tourist</p>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className='col-md-6'>
-                  <div className='row'>
-                    <div className='col-lg-10 offset-lg-1'>
+                </Col>
+                <Col className='col-md-6' md={{ span: 12 }}>
+                  <Row className='row'>
+                    <Col className='col-lg-10 offset-lg-1' lg={{ span: 20 }}>
                       <div className='tour-content'>
                         <h2>We offer worldwise tour plan recently</h2>
                         <p>Make she'd moved divided air. Whose tree that hath own upon them it multiply was blessed </p>
                       </div>
-                    </div>
-                  </div>
+                    </Col>
+                  </Row>
                   <div className='tour-card'>
                     <img className='card-img rounded-0' src='img/home/tour2.png' alt='' />
                     <div className='tour-card-overlay'>
                       <div className='media'>
                         <div className='media-body'>
-                            <h4>Running real time</h4>
-                            <small>5 days offer</small>
-                            <p>We proper guided our tourist</p>
-                          </div>
+                          <h4>Running real time</h4>
+                          <small>5 days offer</small>
+                          <p>We proper guided our tourist</p>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
-              <div className='row'>
-                <div className='col-md-6 col-lg-7'>
+                </Col>
+              </Row>
+              <Row className='row'>
+                <Col className='col-md-6 col-lg-7' md={{ span: 12 }}>
                   <div className='tour-card'>
                     <img className='card-img rounded-0' src='img/home/tour3.png' alt='' />
                     <div className='tour-card-overlay'>
                       <div className='media'>
                         <div className='media-body'>
-                            <h4>Easy to move tasks</h4>
-                            <small>5 days offer</small>
-                            <p>We proper guided our tourist</p>
-                          </div>
+                          <h4>Easy to move tasks</h4>
+                          <small>5 days offer</small>
+                          <p>We proper guided our tourist</p>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className='col-md-6 col-lg-5'>
+                </Col>
+                <Col className='col-md-6 col-lg-5' md={{ span: 12 }}>
                   <div className='tour-card'>
                     <img className='card-img rounded-0' src='img/home/tour4.png' alt='' />
                     <div className='tour-card-overlay'>
                       <div className='media'>
                         <div className='media-body'>
-                            <h4>Friendly interface</h4>
-                            <small>5 days offer</small>
-                            <p>We proper guided our tourist</p>
-                          </div>
+                          <h4>Friendly interface</h4>
+                          <small>5 days offer</small>
+                          <p>We proper guided our tourist</p>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
+                </Col>
+              </Row>
             </div>
           </section>
           {/* ================Tour section End ================= */}
@@ -309,55 +309,55 @@ export default class LandingPage extends Component {
               </div>
               <div className='owl-carousel owl-theme testimonial pb-xl-5'>
                 <div className='testimonial__item'>
-                  <div className='row'>
-                    <div className='col-md-3 col-lg-2 align-self-center'>
+                  <Row className='row'>
+                    <Col className='col-md-3 col-lg-2 align-self-center' md={{ span: 6 }} lg={{ span: 4 }}>
                       <div className='testimonial__img'>
                         <img className='card-img rounded-0' src='img/testimonial/t-slider1.png' alt='' />
                       </div>
-                    </div>
-                    <div className='col-md-9 col-lg-10'>
+                    </Col>
+                    <Col className='col-md-9 col-lg-10' md={{ span: 18 }} lg={{ span: 20 }}>
                       <div className='testimonial__content mt-3 mt-sm-0'>
                         <h3>Daniel heart</h3>
                         <p>Project manager, Nestle</p>
                         <p className='testimonial__i'>Also made from. Give may saying meat there from heaven it lights face had is gathered god earth light for life may itself shall whales made they're blessed whales also made from give may saying meat. There from heaven it lights face had</p>
                         <span className='testimonial__icon'><i className='ti-quote-right' /></span>
                       </div>
-                    </div>
-                  </div>
+                    </Col>
+                  </Row>
                 </div>
                 <div className='testimonial__item'>
-                  <div className='row'>
-                    <div className='col-md-3 col-lg-2 align-self-center'>
+                  <Row className='row'>
+                    <Col className='col-md-3 col-lg-2 align-self-center' md={{ span: 6 }} lg={{ span: 4 }}>
                       <div className='testimonial__img'>
                         <img className='card-img rounded-0' src='img/testimonial/t-slider1.png' alt='' />
                       </div>
-                    </div>
-                    <div className='col-md-9 col-lg-10'>
+                    </Col>
+                    <Col className='col-md-9 col-lg-10' md={{ span: 18 }} lg={{ span: 20 }}>
                       <div className='testimonial__content mt-3 mt-sm-0'>
                         <h3>Daniel heart</h3>
                         <p>Project manager, Nestle</p>
                         <p className='testimonial__i'>Also made from. Give may saying meat there from heaven it lights face had is gathered god earth light for life may itself shall whales made they're blessed whales also made from give may saying meat. There from heaven it lights face had</p>
                         <span className='testimonial__icon'><i className='ti-quote-right' /></span>
                       </div>
-                    </div>
-                  </div>
+                    </Col>
+                  </Row>
                 </div>
                 <div className='testimonial__item'>
-                  <div className='row'>
-                    <div className='col-md-3 col-lg-2 align-self-center'>
+                  <Row className='row'>
+                    <Col className='col-md-3 col-lg-2 align-self-center' md={{ span: 6 }} lg={{ span: 4 }}>
                       <div className='testimonial__img'>
                         <img className='card-img rounded-0' src='img/testimonial/t-slider1.png' alt='' />
                       </div>
-                    </div>
-                    <div className='col-md-9 col-lg-10'>
+                    </Col>
+                    <Col className='col-md-9 col-lg-10' md={{ span: 18 }} lg={{ span: 20 }}>
                       <div className='testimonial__content mt-3 mt-sm-0'>
                         <h3>Daniel heart</h3>
                         <p>Project manager, Nestle</p>
                         <p className='testimonial__i'>Also made from. Give may saying meat there from heaven it lights face had is gathered god earth light for life may itself shall whales made they're blessed whales also made from give may saying meat. There from heaven it lights face had</p>
                         <span className='testimonial__icon'><i className='ti-quote-right' /></span>
                       </div>
-                    </div>
-                  </div>
+                    </Col>
+                  </Row>
                 </div>
               </div>
             </div>
@@ -368,18 +368,18 @@ export default class LandingPage extends Component {
         <Footer className='footer-area'>
           <div className='container'>
             <div className='footer-bottom'>
-              <div className='row align-items-center'>
+              <Row className='row align-items-center'>
                 <p className='col-lg-8 col-sm-12 footer-text m-0 text-center text-lg-left'>{/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
-                  Copyright © All rights reserved | This template is made with <i className='fa fa-heart' aria-hidden='true' /> by <a href='https://colorlib.com' target='_blank'>Colorlib</a>
+                  Copyright © All rights reserved | This template is made with <i className='fa fa-heart' aria-hidden='true' /> by <a href='https://colorlib.com'>Colorlib</a>
                   {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
                 </p>
-                <div className='col-lg-4 col-sm-12 footer-social text-center text-lg-right'>
+                <Col className='col-lg-4 col-sm-12 footer-social text-center text-lg-right' lg={{ span: 8 }} sm={{ span: 24 }}>
                   <a href='#'><i className='fab fa-facebook-f' /></a>
                   <a href='#'><i className='fab fa-twitter' /></a>
                   <a href='#'><i className='fab fa-dribbble' /></a>
                   <a href='#'><i className='fab fa-behance' /></a>
-                </div>
-              </div>
+                </Col>
+              </Row>
             </div>
           </div>
         </Footer>
