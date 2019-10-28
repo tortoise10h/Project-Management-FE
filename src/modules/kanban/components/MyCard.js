398d0251104add720643fa50ebff9b0de2510475
minhminion <minhminion2015@gmail.com>
2019-10-28 21:36:00 +0700

    Create Task Modal

--- src/modules/kanban/components/MyCard.js
+++ src/modules/kanban/components/MyCard.js
@@ -1,66 +1,95 @@
 import React, { Component } from 'react'
-import { Row, Col, Icon, Popconfirm } from 'antd'
-
+import { Row, Col, Icon, Popconfirm, Modal } from 'antd'
+import TaskModal from './TaskModal'
 class MyCard extends Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      visible: false
+    }
+    this.handleShowModal = this.handleShowModal.bind(this)
+    this.handleCloseModal = this.handleCloseModal.bind(this)
+  }
+
+  handleShowModal () {
+    this.setState({
+      visible: true
+    })
+  }
+
+  handleCloseModal () {
+    this.setState({
+      visible: false
+    })
   }
 
   render () {
     const { title, onDelete } = this.props
+    const { visible } = this.state
     return (
-      <div className='trello-card'>
-        <Popconfirm
-          title='Are you sure delete this task?'
-          onConfirm={onDelete}
-          okText='Yes'
-          cancelText='No'
-        >
-          <div className='trello-card--btn'>
-            <Icon type='close' />
-          </div>
-        </Popconfirm>
-        <div className='trello-card--content'>
-          <div className='trello-card--labels'>
-            <span className='trello-card--labels-text' style={{ backgroundColor: '#f17014' }}>
-              test
-            </span>
-          </div>
-          <div className='trello-card--title'>
-            {title || 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam (5)[5]'}
-          </div>
-          <Row className='trello-card--badges'>
-            <Col
-              className='trello-card--badges-item'
-              style={{
-                backgroundColor: '#f5222e'
-              }}
-            >
-              <span>
-                <Icon type='clock-circle' style={{ marginRight: 5, fontSize: 18 }} />
+      <>
+        <div className='trello-card' onClick={this.handleShowModal}>
+          <Popconfirm
+            title='Are you sure delete this task?'
+            onConfirm={onDelete}
+            okText='Yes'
+            cancelText='No'
+          >
+            <div className='trello-card--btn'>
+              <Icon type='close' />
+            </div>
+          </Popconfirm>
+          <div className='trello-card--content'>
+            <div className='trello-card--labels'>
+              <span className='trello-card--labels-text' style={{ backgroundColor: '#f17014' }}>
+                test
               </span>
-              <span>
-                Due Date
-              </span>
-            </Col>
-          </Row>
-          <div className='trello-card--members'>
-            <ul>
-              <li>
-                <div className='trello-card--member'>
-                  <img src={require('./../../../assets/images/landingpage/user/avatar1.png')} />
-                </div>
-              </li>
-              <li>
-                <div className='trello-card--member'>
-                  <img src={require('./../../../assets/images/landingpage/user/avatar2.png')} />>
-                </div>
-              </li>
-            </ul>
+            </div>
+            <div className='trello-card--title'>
+              {title || 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam (5)[5]'}
+            </div>
+            <Row className='trello-card--badges'>
+              <Col
+                className='trello-card--badges-item'
+                style={{
+                  backgroundColor: '#f5222e'
+                }}
+              >
+                <span>
+                  <Icon type='clock-circle' style={{ marginRight: 5, fontSize: 18 }} />
+                </span>
+                <span>
+                  Due Date
+                </span>
+              </Col>
+            </Row>
+            <div className='trello-card--members'>
+              <ul>
+                <li>
+                  <div className='trello-card--member'>
+                    <img src={require('./../../../assets/images/landingpage/user/avatar1.png')} />
+                  </div>
+                </li>
+                <li>
+                  <div className='trello-card--member'>
+                    <img src={require('./../../../assets/images/landingpage/user/avatar2.png')} />>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
+        <Modal
+          className='task-modal'
+          title={title}
+          visible={visible}
+          footer={null}
+          onCancel={this.handleCloseModal}
+          width='60%'
+        >
+          <TaskModal />
+        </Modal>
+      </>
     )
   }
 }
