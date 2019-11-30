89d60318303e48ae29fd1989710843fae0674719
LuongTuLinh <hoailinhkt2015@gmail.com>
2019-11-30 19:46:07 +0700

    fix edit project completed

--- src/modules/settingProject/models.js
+++ src/modules/settingProject/models.js
@@ -3,7 +3,12 @@ import { DEFAULT_URL } from '../../configs'
 export const MODULE_NAME = 'kanban'
 export const ENDPOINTS = {
   getProjectInfo: (projectId, userId) => `${DEFAULT_URL}/project/${projectId}`,
-  updateProject: (projectId) => `${DEFAULT_URL}/project/${projectId}`
+  updateProject: (projectId) => `${DEFAULT_URL}/project/${projectId}`,
+  updateTitleProject: (projectId) => `${DEFAULT_URL}/project/${projectId}`,
+  updateStatusProject: (projectId) => `${DEFAULT_URL}/project/${projectId}`,
+  updateDescriptionProject: (projectId) => `${DEFAULT_URL}/project/${projectId}`,
+  updateStartDateProject: (projectId) => `${DEFAULT_URL}/project/${projectId}`,
+  updateEndDateProject: (projectId) => `${DEFAULT_URL}/project/${projectId}`
 }
 
 export const LIMIT = 20
